0afa80511bf35589f9dc7e9e2991db6ca8f920a7
Alaor Barroso de Carvalho Neto <alaorneto@petrobras.com.br>
2020-06-14 13:22:52 -0300

    #1 Criadas rotinas específicas para empresas.

--- codigo/nuvem_palavras.py
+++ codigo/nuvem_palavras.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
 
 from wordcloud import WordCloud
 from PIL import Image
@@ -10,6 +7,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import nltk
+import operator
+
 
 def ler_arquivo(arquivo):
     with open(arquivo, mode='r') as txt_file:
@@ -23,16 +22,32 @@ def ler_arquivo(arquivo):
 def gerar_nuvem(texto):
     maskArray = np.array(Image.open("cloud.png"))
     wordcloud = WordCloud(collocations=False, background_color = "white", max_words = 300, mask = maskArray)
-    #wordcloud = WordCloud(regexp="\w[\w_]+", collocations=False, width=800, height=600, max_words=20)
-    wordcloud.generate(texto)
+
+    texto_arr = texto.split()
+
+    for index, nome in enumerate(texto_arr):
+        texto_arr[index] = nome.replace("Î", " ")
+
+    freq_dict = dict.fromkeys(texto_arr)
+
+    for e in freq_dict:
+        freq_dict[e] = texto_arr.count(e)
+    
+    maximo = max(freq_dict.items(), key=operator.itemgetter(1))[1]
+
+    for e in freq_dict:
+        freq_dict[e] = freq_dict[e] / maximo
+
+    wordcloud.generate_from_frequencies(freq_dict)
     
     plt.figure(figsize=(12,10))
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis("off")
     plt.show()
 
+
 def ler_dados(arquivo):
-    dados = pd.read_csv(arquivo, sep=';', encoding='utf-8')
+    dados = pd.read_csv(arquivo, sep=';', encoding='latin-1')
     return dados
 
    
@@ -60,13 +75,37 @@ def processar(dados, minhas_stopwords):
     return texto_processado
 
 
+"""
+Aglutina as palavras que compõe o nome de cada empresa em uma única,
+utilizando um caractére especial, que posteriormente será removido no
+momento da geração da nuvem de palavras.
+"""
+def fix_subsidiarias(dados):
+    for index, row in dados.iterrows():
+        if row['subsidiaria'] == 'NULL' or row['subsidiaria'] == None or pd.isna(row['subsidiaria']):
+            if not pd.isna(row['empresa']):
+                emp = row['empresa']
+                row['subsidiaria'] = emp.strip().replace(" ", "Î")
+        else:
+            sub = row['subsidiaria']
+            sub = sub.split("/")[0].strip()
+            row['subsidiaria'] = sub.replace(" ", "Î")
+    return dados
 
-''' Main program '''
+
+''' 
+Executa o programa. A variável 'tipo_nuvem' deve receber uma string
+correspondente a uma das colunas do arquivo de entrada.
+'''
 def main():
-    
+    tipo_nuvem = 'subsidiaria'
     minhas_stopwords = ler_arquivo("minhas_stopwords.txt")
     dados = ler_dados("reclamacoes.csv")
-    conteudo = obter_campo(dados, 'serviço')
+
+    if tipo_nuvem == 'subsidiaria':
+        dados = fix_subsidiarias(dados)
+    
+    conteudo = obter_campo(dados, tipo_nuvem)
 
     texto = processar(conteudo, minhas_stopwords)
     gerar_nuvem(texto)
@@ -74,10 +113,5 @@ def main():
     del dados
     del texto
     
-    
-    
-    
-    
-
 if __name__ == "__main__":
     sys.exit(main())
